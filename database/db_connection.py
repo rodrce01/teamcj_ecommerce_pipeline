56b9664f1622c662417485b6123c8d8eef1ee4ee
rodrce01 <rodrce01@uwgb.edu>
2025-11-30 21:49:42 -0600

    Working on MongoDB connection

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,1 +1,20 @@
 #for our mongoDB connection and setup
+
+import os
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# load .env
+load_dotenv()
+
+#get the Mongo URI from environment variables
+uri = os.getenv("MONGO_URI")
+if not uri:
+    raise ValueError("No MONGO_URI found. Please set it in your .env file")
+
+#connect to MongoDB
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+#set the project database
+db = client['EcommerceProject']
--- processing/analysis.py
+++ processing/analysis.py
@@ -1,2 +1,13 @@
 #market basket analysis / trend detections
 #we could use scikit-learn, pandas, etc.
+
+from db_connection import db
+
+def get_top_categories():
+    #example funct to get top product categories
+    collection = db['cleaned_products']
+    pipeline = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}
+    ]
+    return list(collection.aggregate(pipeline))
